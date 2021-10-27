25b627813dd57a96376aa2b834506b04fbe7bf33
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-10-27 15:52:56 +0000

    revise

--- smallest_number.py
+++ smallest_number.py
@@ -10,14 +10,9 @@ def calculated_smallest_number(calculate_my_numbers):
     # This function calculate smallest number
     loop_number_second = 0
     total_number = 100
-    time = 0
 
     # process
     for loop_number_second in calculate_my_numbers:
-        time += 1
-        # output
-        print("The random number {0} is {1}".format(time, loop_number_second))
-
         if loop_number_second < total_number:
             total_number = loop_number_second
 
@@ -30,14 +25,20 @@ def main():
     smallest_number = 0
     loop_number_first = 0
 
+    # output
+    print("Here is a list of random numbers:")
+    print("")
+
     # process
     for loop_number_first in range(0, 10):
         calculate_number = random.randint(0, 100)
         my_numbers.append(calculate_number)
-
-    # output
-    print("Here is a list of random numbers:")
-    print("")
+        # output
+        print(
+            "The random number {0} is {1}".format(
+                loop_number_first + 1, calculate_number
+            )
+        )
 
     # call functions
     smallest_number = calculated_smallest_number(my_numbers)
